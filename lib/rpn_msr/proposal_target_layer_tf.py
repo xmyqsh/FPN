fix the calculation of leveled rois

lib/rpn_msr/proposal_target_layer_tf.py
@@ -104,10 +104,10 @@ def proposal_target_layer(rpn_rois, gt_boxes, gt_ishard, dontcare_areas, _num_cl
     level = lambda roi : calc_level(roi[3] - roi[1], roi[4] - roi[2])   # roi: [0, x0, y0, x1, y1]
 
     leveled_rois = [[], [], [], []]
-    leveled_rois[0] = [roi for level, roi in zip(levels, rois) if level(roi) == 2]
-    leveled_rois[1] = [roi for level, roi in zip(levels, rois) if level(roi) == 3]
-    leveled_rois[2] = [roi for level, roi in zip(levels, rois) if level(roi) == 4]
-    leveled_rois[3] = [roi for level, roi in zip(levels, rois) if level(roi) == 5]
+    leveled_rois[0] = [roi for roi in rois if level(roi) == 2]
+    leveled_rois[1] = [roi for roi in rois if level(roi) == 3]
+    leveled_rois[2] = [roi for roi in rois if level(roi) == 4]
+    leveled_rois[3] = [roi for roi in rois if level(roi) == 5]
 
     leveled_rois[0] = np.array(leveled_rois[0])
     leveled_rois[1] = np.array(leveled_rois[1])