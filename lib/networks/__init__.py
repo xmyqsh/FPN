aa8f3bfff68297ce8318ed15c19df0f2ab968e60
xmyqsh <xmyqsh@gmail.com>
2017-05-17 10:34:54 +0800

    network

---
+++ lib/networks/__init__.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+from .FPN_train import FPN_train
+from .FPN_test import FPN_test
+from . import factory
---
+++ lib/networks/factory.py
@@ -0,0 +1,29 @@
+# --------------------------------------------------------
+# SubCNN_TF
+# Copyright (c) 2016 CVGL Stanford
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Yu Xiang
+# --------------------------------------------------------
+
+"""Factory method for easily getting imdbs by name."""
+
+__sets = {}
+
+from .FPN_train import FPN_train
+from .FPN_test import FPN_test
+
+def get_network(name):
+    """Get a network by name."""
+    if name.split('_')[0] == 'FPN':
+        if name.split('_')[1] == 'test':
+           return FPN_test()
+        elif name.split('_')[1] == 'train':
+           return FPN_train()
+        else:
+           raise KeyError('Unknown dataset: {}'.format(name))
+    else:
+        raise KeyError('Unknown dataset: {}'.format(name))
+
+def list_networks():
+    """List all registered imdbs."""
+    return __sets.keys()
